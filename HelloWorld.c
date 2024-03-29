e15330f44f7091ec9ff46bb93707b695d775902a
The Moose Man <obtusemoose1995@gmail.com>
2024-03-29 10:13:18 +0900

    Initial Commit Hello World

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+/*
+Author: Dustin Warren
+Handle: The Moose Man
+Date: March 29, 2024
+
+Program Description: This program is my debut into the public world 
+                     of C programming and sharing my work on GitHub
+*/
+
+//#include <stdio.h>
+
+int main() {
+    printf("Hello World!");
+    return 0;
+}
